80a98cc9ec1ac7a10f4a0943cf86fed98f6caff0
mcyaxVillanueva@gmail.com <mcyaxVillanueva@gmail.com@8c90b02a-0569-2457-1a9c-a2a90d6cf29a>
2013-10-09 06:05:20 +0000

    UI main

---
+++ view_book.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Testing...</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script type="text/javascript" language="javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script type="text/javascript" language="javascript" src="<?=base_url()?>resources/js/jsqueries.js"></script>
+       
+    </head>
+    <body>
+        <div id="upper" style="left: 268px; position: relative; top: 29px; width: auto;">
+            <input type="text" id="input" required placeholder="Enter ISBN number here..."> 
+            <input type="text" id="api" required placeholder="Authorized?">
+            <button id="search">Search</button>
+        </div>
+        <div id="display" style="left: 150px; position: relative; top: 40px;">
+            <table id="put_here">
+                <tr><th colspan="2">Details</th><tr>
+            </table>
+        </div>
+    </body>
+</html>
