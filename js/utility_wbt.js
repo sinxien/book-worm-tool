0b345e5d542abd4504f65eae547df9fd9e8d8be1
mcyaxVillanueva@gmail.com <mcyaxVillanueva@gmail.com@8c90b02a-0569-2457-1a9c-a2a90d6cf29a>
2013-10-09 07:54:07 +0000

    edited for nested objects

--- js/utility_wbt.js
+++ js/utility_wbt.js
@@ -4,15 +4,23 @@
   
        $('#search').click(function(){
                            var isbn = $('#input').val();
-
+//                           var api = $('#api').val();
                            var html = '';
                                      
                              $.getJSON("https://www.googleapis.com/books/v1/volumes?q="+ isbn + "&key=AIzaSyApOf-hpK40eylVFgRUEEEFrvc9ohPKfOI", 
                                 function(data) {                               
                                     
-                                $.each(data.volumeInfo, function(key,value){
-                                    html = '<tr><td><b>'+key+'</b></td><td style="color:#ABCDEF;"><i>'+value+'</i></td></tr>';
-                                    $('#put_here').append(html);
+                                $.each(data.items[0].volumeInfo, function(key,value){
+                                    if ( key === 'industryIdentifiers' ) {
+                                       $.each(data.items[0].volumeInfo.industryIdentifiers, function(name,data_val){
+                                            html = '<tr><td><b>'+key+' -> '+data_val.type+'</b></td><td><i>'+data_val.identifier+'</i></td></tr>';
+                                            $('#put_here').append(html);
+                                        });
+                                    } else {
+                                        html = '<tr><td><b>'+key+'</b></td><td><i>'+value+'</i></td></tr>';
+                                        $('#put_here').append(html);
+                                    }                                    
+                                    
                                 });
                                     
                             });
