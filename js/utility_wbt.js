640b55c20b6e3c8d92446a17807e31dfd30480c5
mcyaxVillanueva@gmail.com <mcyaxVillanueva@gmail.com@8c90b02a-0569-2457-1a9c-a2a90d6cf29a>
2013-10-09 06:48:26 +0000

    edited the loop for the json data fetched

--- js/utility_wbt.js
+++ js/utility_wbt.js
@@ -3,19 +3,21 @@
   $(document).ready(function(){
   
        $('#search').click(function(){
-             var isbn = $('#input').val();
-             var api = $('#api').val();
-             var html = '';
+                           var isbn = $('#input').val();
+
+                           var html = '';
                                      
-           $.getJSON("http://isbndb.com/api/v2/json/"+api+"/books?q="+ isbn + "?callback=?", 
-                function(data) {                               
-                    
-                $.each($.parseJSON(data), function(key,value){
-                  html = '<tr><td><b>'+key+'</b></td><td style="color:#ABCDEF;"><i>'+value+'</i></td></tr>';
-                    $('#put_here').append(html);
-                });
-                    
-            });
+                             $.getJSON("https://www.googleapis.com/books/v1/volumes?q="+ isbn + "&key=AIzaSyApOf-hpK40eylVFgRUEEEFrvc9ohPKfOI", 
+                                function(data) {                               
+                                    
+                                $.each(data.volumeInfo, function(key,value){
+                                    html = '<tr><td><b>'+key+'</b></td><td style="color:#ABCDEF;"><i>'+value+'</i></td></tr>';
+                                    $('#put_here').append(html);
+                                });
+                                    
+                            });
+                        
+                       });
         
        });
   
