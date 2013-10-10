c0a83f931a1075f278c149cdbe76782b465e9fd9
mcyaxVillanueva@gmail.com <mcyaxVillanueva@gmail.com@8c90b02a-0569-2457-1a9c-a2a90d6cf29a>
2013-10-10 00:34:44 +0000

    edited html string to emphasize per book

--- js/utility_wbt.js
+++ js/utility_wbt.js
@@ -4,28 +4,33 @@
   
        $('#search').click(function(){
                            var isbn = $('#input').val();
-//                           var api = $('#api').val();
                            var html = '';
+						   $('#put_here').html();
                                      
                              $.getJSON("https://www.googleapis.com/books/v1/volumes?q="+ isbn + "&key=AIzaSyApOf-hpK40eylVFgRUEEEFrvc9ohPKfOI", 
                                 function(data) {                               
                                     
-                                $.each(data.items[0].volumeInfo, function(key,value){
-                                    if ( key === 'industryIdentifiers' ) {
-                                       $.each(data.items[0].volumeInfo.industryIdentifiers, function(name,data_val){
-                                            html = '<tr><td><b>'+key+' -> '+data_val.type+'</b></td><td><i>'+data_val.identifier+'</i></td></tr>';
-                                            $('#put_here').append(html);
-                                        });
-                                    } else {
-                                        html = '<tr><td><b>'+key+'</b></td><td><i>'+value+'</i></td></tr>';
-                                        $('#put_here').append(html);
-                                    }                                    
-                                    
-                                });
-                                    
-                            });
-                        
-                       });
+								$.each(data.items, function(i,name){
+									
+										$.each(data.items[i].volumeInfo, function(key,value){
+											if ( key === 'industryIdentifiers' ) {
+											   $.each(data.items[i].volumeInfo.industryIdentifiers, function(name,data_val){
+													html = '<tr><td><b>'+key+'  '+data_val.type+'</b></td><td><i>'+data_val.identifier+'</i></td></tr>';
+													$('#put_here').append(html);
+												});
+											} else if ( key === 'title' ) {
+												html = '<tr><td colspan="2"><hr style="width: 100%;"></td></tr><tr><td><b>'+key+'</b></td><td><i>'+value+'</i></td></tr>';
+												$('#put_here').append(html);
+											} else {
+												html = '<tr><td><b>'+key+'</b></td><td><i>'+value+'</i></td></tr>';
+												$('#put_here').append(html);
+											}                                    
+											
+										});
+									
+								});
+                    
+            });
         
        });
   
